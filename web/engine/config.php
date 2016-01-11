86b1e50a4cb7a4b205db487fa89c61386b544191
CoderX <demix@bigmir.net>
2016-01-11 18:13:37 +0200

    config

---
+++ web/engine/config.php
@@ -0,0 +1,19 @@
+<?php
+if(!defined("IN_RULE")) die ("Oops");
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('TIMEZONE', '');
+
+define('TABLE_USERS', '');
+define('TABLE_SESSIONS', '');
+define('TABLE_TMPLOGS', '');
+
+define('RECAPTCHA_PRIVATKEY', '');
+define('RECAPTCHA_SITEKEY', '');
+
+define('SITE_DIRECTORY', 	'');
+define('CSS_PATH', 			'/assets/css/');
+define('JS_PATH', 			'/assets/js/');
+
+?>
